571093958b34a37672e19e40a38aae6f98c81f6d
Nigel <nigelchristiaans747@gmail.com>
2018-09-03 13:12:56 +0200

    now has notification controll

---
+++ app/src/main/java/development/iwa/firetimer/AppConstants.kt
@@ -0,0 +1,10 @@
+package development.iwa.firetimer
+
+class AppConstants {
+    companion object {
+        const val ACTION_STOP = "stop"
+        const val ACTION_PAUSE = "pause"
+        const val ACTION_RESUME = "resume"
+        const val ACTION_START = "start"
+    }
+}
